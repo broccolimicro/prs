d5d1ab7476a32636505e345ce01648ba365fcd50
Ned Bingham <edward.bingham@broccolimicro.io>
2024-09-18 15:16:00 -0400

    adding spice export

--- prs/synthesize.cpp
+++ prs/synthesize.cpp
@@ -1,39 +1,57 @@
 #include "synthesize.h"
+#include <cmath>
+#include <interpret_ucs/export.h>
 
 namespace prs {
 
-sch::Subckt build_netlist(const production_rule_set &prs) {
-	// TODO Circuit and Netlist Synthesis
-	//
-	// The goal of this project is to generate a spice netlist from the
-	// production rule set. With this spice netlist, we'll be able to divide the
-	// netlist up into gates and then run those gates through Floret for cell
-	// generation.
-	//
-	// The following steps are guidelines and not hard rules. If you think you
-	// found a better way to approach the problem, then feel free to chase that
-	// down. If you need supporting infrastructure anywhere else in the project,
-	// feel free to add that in. If you need to modify this function definition,
-	// go for it.
-	//
-	// 1. Export production rule set to a spice process
-	// 2. Automatically determine transistor sizing using logical effort
-	// 3. Expand upon this to implement optimal QDI transistor sizing
-	// 4. Update production rules to fold transistors
-	// 5. Determine optimal folding based on state space
-	// 6. Automatically break a production rule set into cells for layout
-	//
-	// === Successful completion of project ===
-	// 
-	// Your time is yours, what do you want to tackle next?
-	// Some ideas:
-	// 1. Use state graph to determine optimal transistor length to minimize
-	//    leakage current while maintaining performance.
-	//
-	// Final cleanup:
-	// 1. Clean up any bugs
-	// 2. Prepare demo
-	// 3. Document as needed
+sch::Subckt build_netlist(const phy::Tech &tech, const production_rule_set &prs, const ucs::variable_set &v) {
+	sch::Subckt result(tech);
+
+	result.name = "top";
+	for (int i = 0; i < (int)prs.nets.size(); i++) {
+		// TODO(edward.bingham) we gotta figure out IO
+		result.nets.push_back(sch::Net(export_variable_name(i, v).to_string(), false));
+	}
+
+	for (int i = -1; i >= -(int)prs.nodes.size(); i--) {
+		result.nets.push_back(sch::Net(export_variable_name(i, v).to_string(), false));
+	}
+
+	int minLength = tech.paint[tech.wires[0].draw].minWidth;
+
+	for (auto dev = prs.devs.begin(); dev != prs.devs.end(); dev++) {
+		int model = 0;
+		for (model = 0; model < (int)tech.models.size(); model++) {
+			auto m = tech.models.begin()+model;
+			if (((m->type == phy::Model::NMOS and dev->threshold == 1)
+					or (m->type == phy::Model::PMOS and dev->threshold == 0))
+				and ((dev->attr.variant == "" and m->name.find("svt") != string::npos) or (
+					m->name.find(dev->attr.variant) != string::npos))) {
+				break;
+			}
+		}
+		if (model >= (int)tech.models.size()) {
+			error("", std::string(dev->threshold == 1 ? "nmos" : "pmos") + " transistor variant " + (dev->attr.variant == "" ? "svt" : dev->attr.variant) + " not found.", __FILE__, __LINE__);
+			continue;
+		}
+		int type = tech.models[model].type;
+		int minWidth = tech.paint[tech.models[model].paint[0].draw].minWidth*3;
+
+		cout << "adding model " << model << " of type " << type << " with name " << tech.models[model].name << endl;
+		result.mos.push_back(sch::Mos(model, type));
+		result.mos.back().gate = prs.uid(dev->gate);
+		result.mos.back().ports.push_back(prs.uid(dev->source));
+		result.mos.back().ports.push_back(prs.uid(dev->drain));
+		result.mos.back().bulk = prs.pwr[0][1-dev->threshold];
+		if (dev->attr.size < 1.0) {
+			result.mos.back().size[0] = (int)ceil(((float)minLength)/dev->attr.size);
+			result.mos.back().size[1] = minWidth;
+		} else {
+			result.mos.back().size[0] = minLength;
+			result.mos.back().size[1] = (int)ceil(dev->attr.size*(float)minWidth);
+		}
+	}
+	return result;
 }
 
 }
--- prs/synthesize.h
+++ prs/synthesize.h
@@ -5,7 +5,7 @@
 
 namespace prs {
 
-sch::Subckt build_netlist(const production_rule_set &prs);
+sch::Subckt build_netlist(const phy::Tech &tech, const production_rule_set &prs, const ucs::variable_set &v);
 
 }
 
